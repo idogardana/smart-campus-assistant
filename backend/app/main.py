5db89781e506514a8442c35b4274b2e7c9413aea
idogardana <idogardana21@gmail.com>
2026-04-14 17:21:34 +0300

    add github actions workflow

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .router import router
 
-app = FastAPI(title="Smart Campus Assistant")
+app = FastAPI(title="Smart Campus Assistant API")
 
 app.add_middleware(
     CORSMiddleware,
